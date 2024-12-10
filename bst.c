085cf429880d894fbd85245d84517d3e2a4e8da3
EsmeraldaJardine <2391491j@student.gla.ac.uk>
2024-12-10 15:35:05 +0000

    last working version

--- bst.c
+++ bst.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 
 typedef struct node {
     int value;             // The value stored in the node
@@ -37,8 +37,7 @@ node_t * search(node_t * node, int toFind){
 };
 
 void insert(node_t *node, int elem){
-    if (elem == node->value || node == NULL) {
-        printf("Error \n");
+    if (elem == node->value) {
         return;
     }
     // left subtree
@@ -78,41 +77,73 @@ void destroyTree(node_t * node){
 };
 
 void delete(node_t * node, int elem){
-    if (node == NULL){return;}
+    node_t *parent = NULL;
+    node_t *current = node;
+    bool hasLeftChild = false;
     //traversal
-    if (elem < node->value){
-        delete(node->left, elem);
-    }
-    else if (elem > node->value){
-        delete(node->right, elem);
-    }
-    else{
-        //leaf node
-        if (elem == node->value && node->left == NULL && node->right == NULL){
-        printf("Deleting leaf node\n");
-        free(node);
-        return;
-        } 
-        //node with one child
-        if (elem == node->value && node->left != NULL || node->right != NULL){
-            printf("Deleting node with one child\n");
-            node_t *child = (node->left != NULL) ? node->left : node->right; //child pointer
-            node->value = child->value;
-            node->left = child->left;
-            node->right = child->right;
-            free(child);
+    while (current != NULL && current->value != elem){
+        parent = current;
+        if (elem < current->value){
+            current = current->left;
+            hasLeftChild = true;
+        }else{
+            current = current->right;
+            hasLeftChild = false;
         }
+    }
+    if (current == NULL){return;}
+
+    // leaf node 
+    if (current->left == NULL && current->right == NULL) {
+        // root node?
+        if (current == node) {
+            free(current); 
+            node = NULL;
+            return;
+        }
+        if (hasLeftChild)
+            {parent->left = NULL;} 
+        else 
+            {parent->right = NULL;}
+        free(current);
+        return; //this is not doing the right thing as leaf node has no children
+    } 
+    // node with one child
+    if (current->left == NULL || current->right == NULL) {
+        node_t *child = (current->left == NULL) ? current->right : current->left;
+        
+        //root node?
+        if (current == node) {
+            node = child;
+            free(current);
+            return  ;
+        }
+        
+        if (hasLeftChild)
+            {parent->left = child;}
+        else
+            {parent->right = child;}
+        
+        free(current);
+        return;
+    }
+
+    else {
         //node with two children
-        if (elem == node->value && node->left != NULL && node->right != NULL){
-            printf("Deleting node with two children\n");
-            node_t *min_elem = node->right;
+        printf("Deleting node with two children\n");
+            node_t *min_elem = current->right;
             while (min_elem->left != NULL){
                 min_elem = min_elem->left;
             }
-            node->value = min_elem->value;
-            delete(node->right, min_elem->value);
-        }
+            current->value = min_elem->value;
+            delete(current->right, min_elem->value);
+
+
     }
+
+    
+
+        
 };
     
 
@@ -122,8 +153,6 @@ int main(){
     insert(root, 5);
     insert(root, 12);
     insert(root, 5);
-    printf("%d\n", root->left->value);
-    printf("%d\n", root->right->value);
     search(root, 5);
     search(root, 12);
     search(root, 22);
